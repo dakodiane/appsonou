4ffc0e2426706faf3559106a92d9928646109e21
dakodiane <dakodiane470@gmail.com>
2023-08-23 15:42:22 +0100

    crshowupdate

--- app/Http/Controllers/BonCaisseController.php
+++ app/Http/Controllers/BonCaisseController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BonCaisse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class BonCaisseController extends Controller
 {
@@ -14,6 +17,28 @@ class BonCaisseController extends Controller
     public function index()
     {
         //
+
+    }
+    public function createFacture(Request $request)
+    {
+        // Valider les données d'entrée pour la création de factures
+        $request->validate([
+            'montant_facture' => 'required|numeric',
+            'date_emission' => 'required|date',
+            'nature_retenue' => 'required|in:prestation,achat',
+        ]);
+
+        // Créez et sauvegardez le bon de caisse de type "facture"
+        $bon = new BonCaisse;
+        $bon->type = 'facture';
+        $bon->montant_facture = $request->montant_facture;
+        $bon->date_emission = $request->date_emission;
+        $bon->nature_retenue = $request->nature_retenue;
+        $bon->user_id = 1;
+
+
+        $bon->save();
+        return response()->json(['message' => 'Facture enregistrée avec succès']);
     }
 
     /**
@@ -21,10 +46,6 @@ class BonCaisseController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -32,9 +53,109 @@ class BonCaisseController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function storeFacture(Request $request)
     {
         //
+        $validator = Validator::make($request->all(), [
+            'id' => 'required|exists:bon_caisses',
+            'categorie_id' => 'required|exists:categories,id',
+            'beneficiaire_id' => 'required|exists:beneficiaires,id',
+            'motif_operation' => 'required',
+            'montant_facture' => 'required|numeric',
+            'montant_imposable' => 'required|numeric',
+            'montant_aib' => 'required|numeric',
+            'taux_ab' => 'required|in:0.01,0.03,0.05',
+            'avance_perçue' => 'required|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        // Recherchez la facture existante
+        $facture = BonCaisse::where('id', $request->id)->first();
+
+        if (!$facture) {
+            return response()->json(['message' => 'Facture non trouvée.'], 404);
+        }
+        // Calculer le montant AIB
+        $montantAIB = $request->montant_imposable * $request->taux_ab;
+
+        // Calculer le montant net à payer
+        $montantNetAPayer = $request->montant_facture - $montantAIB;
+
+        // Vérifier si le montant net à payer est inférieur à 100 000
+        if ($montantNetAPayer > 100000) {
+            return response()->json(['message' => 'Le montant net à payer dépasse 100 000.'], 400);
+        }
+
+        // Ajoutez les détails du paiement à la facture
+        $facture->beneficiaire_id = $request->beneficiaire_id;
+        $facture->montant_facture = $request->montant_facture;
+        $facture->motif_operation = $request->motif_operation;
+        $facture->date_emission = $request->date_emission;
+        $facture->avance_perçue = $request->avance_perçue;
+        $facture->montant_imposable = $request->montant_imposable;
+        $facture->taux_ab = $request->taux_ab;
+        $facture->montant_aib = $montantAIB;
+        $facture->montant_paye = $montantNetAPayer;
+
+        $facture->save();
+
+        return response()->json(['message' => 'Paiement de facture enregistré avec succès']);
+    }
+
+
+    public function createAvance(Request $request)
+    {
+        //
+        $request->validate([
+            'categorie_id' => 'required|exists:categories,id',
+            'beneficiaire_id' => 'required|exists:beneficiaires,id',
+            'montant_facture' => 'required|numeric',
+            'avance_perçue' => 'required|numeric',
+            'date_emission' => 'required|date',
+            'motif_operation' => 'required',
+        ]);
+
+        // Créez et sauvegardez le bon de caisse de type "facture"
+        $avance = new BonCaisse;
+        $avance->type = 'avance';
+        $avance->beneficiaire_id = $request->beneficiaire_id;
+        $avance->montant_facture = $request->montant_facture;
+        $avance->date_emission = $request->date_emission;
+        $avance->avance_perçue = $request->avance_perçue;
+        $avance->motif_operation = $request->motif_operation;
+        $avance->user_id = 1;
+
+
+        $avance->save();
+        return response()->json(['message' => 'Paiement d/avance effectué avec succès']);
+    }
+
+    public function createAutres(Request $request)
+    {
+        //
+        $request->validate([
+            'categorie_id' => 'required|exists:categories,id',
+            'beneficiaire_id' => 'required|exists:beneficiaires,id',
+            'montant_facture' => 'required|numeric',
+            'date_emission' => 'required|date',
+            'motif_operation' => 'required',
+        ]);
+
+        // Créez et sauvegardez le bon de caisse de type "facture"
+        $avance = new BonCaisse;
+        $avance->type = 'autres';
+        $avance->beneficiaire_id = $request->beneficiaire_id;
+        $avance->montant_facture = $request->montant_facture;
+        $avance->date_emission = $request->date_emission;
+        $avance->motif_operation = $request->motif_operation;
+        $avance->user_id = 1;
+
+
+        $avance->save();
+        return response()->json(['message' => 'Paiement effectué avec succès']);
     }
 
     /**
@@ -43,10 +164,36 @@ class BonCaisseController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($date_emission, $beneficiaire_id)
     {
-        //
+        // Recherchez tous les bons de caisse correspondant à la date et au bénéficiaire
+        $bons = BonCaisse::where('date_emission', $date_emission)
+            ->where('beneficiaire_id', $beneficiaire_id)
+            ->get();
+        
+        if ($bons->isEmpty()) {
+            return response()->json(['message' => 'Aucun bon de caisse trouvé'], 404);
+        }
+        
+        $bonsInfo = [];
+        
+        foreach ($bons as $bon) {
+            $numeroBon = $bon->id;
+            $motif = $bon->motif_operation;
+            $dateEmission = $bon->date_emission;
+            
+            // Ajoutez les informations de chaque bon au tableau
+            $bonsInfo[] = [
+                'id' => $numeroBon,
+                'motif_operation' => $motif,
+                'date_emission' => $dateEmission
+            ];
+        }
+        
+        return response()->json($bonsInfo);
     }
+    
+    
 
     /**
      * Show the form for editing the specified resource.
@@ -66,11 +213,28 @@ class BonCaisseController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $id, $beneficiaire_id)
     {
-        //
+        // Recherchez le bon de caisse en fonction de son ID et du bénéficiaire
+        $bon = BonCaisse::where('id', $id)
+            ->where('beneficiaire_id', $beneficiaire_id)
+            ->first();
+        
+        if (!$bon) {
+            return response()->json(['message' => 'Bon de caisse introuvable'], 404);
+        }
+        
+        // Mettez à jour les attributs du bon de caisse en fonction de la demande
+      
+        $bon->date_emission = $request->date_emission;
+        $bon->montant_facture = $request->montant_facture;
+        // Assignez les autres attributs de la même manière...
+    
+        $bon->save();
+    
+        return response()->json(['message' => 'Bon de caisse mis à jour avec succès']);
     }
-
+    
     /**
      * Remove the specified resource from storage.
      *
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('bons_caisse/facture', 'App\Http\Controllers\BonCaisseController@createFacture');
+Route::post('facture', 'App\Http\Controllers\BonCaisseController@storeFacture');
+Route::post('bons_caisse/avance', 'App\Http\Controllers\BonCaisseController@createAvance');
+Route::post('bons_caisse/autres', 'App\Http\Controllers\BonCaisseController@createAutres');
+Route::get('/bons_caisse/{date_emission}/{beneficiaire_id}/show', 'App\Http\Controllers\BonCaisseController@show');
+Route::put('/bons_caisse/{id}/{beneficiaire_id}/update', 'App\Http\Controllers\BonCaisseController@update');
+
